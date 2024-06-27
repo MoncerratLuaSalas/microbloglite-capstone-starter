0799603f238f2a9f2ebb6b08a615db2725779b3a
MoncerratLuaSalas <166437793+MoncerratLuaSalas@users.noreply.github.com>
2024-06-27 01:51:39 -0700

    created registration page
    created registration page

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,5 +1,3 @@
-/* auth.js provides LOGIN-related functions */
-
 "use strict";
 
 const apiBaseURL = "http://microbloglite.us-east-2.elasticbeanstalk.com";
@@ -7,7 +5,6 @@ const apiBaseURL = "http://microbloglite.us-east-2.elasticbeanstalk.com";
 
 // NOTE: API documentation is available at /docs 
 // For example: http://microbloglite.us-east-2.elasticbeanstalk.com/docs
-
 
 // You can use this function to get the login data of the logged-in
 // user (if any). It returns either an object including the username
@@ -17,27 +14,22 @@ function getLoginData () {
     return JSON.parse(loginJSON) || {};
 }
 
-
 // You can use this function to see whether the current visitor is
-// logged in. It returns either `true` or `false`.
+// logged in. It returns either true or false.
 function isLoggedIn () {
     const loginData = getLoginData();
     return Boolean(loginData.token);
 }
 
-
 // This function is already being used in the starter code for the
 // landing page, in order to process a user's login. READ this code,
-// and feel free to re-use parts of it for other `fetch()` requests
+// and feel free to re-use parts of it for other fetch() requests
 // you may need to write.
 function login (loginData) {
     // POST /auth/login
     const options = { 
         method: "POST",
         headers: {
-            // This header specifies the type of content we're sending.
-            // This is required for endpoints expecting us to send
-            // JSON data.
             "Content-Type": "application/json",
         },
         body: JSON.stringify(loginData),
@@ -51,21 +43,23 @@ function login (loginData) {
                 // Here is where you might want to add an error notification 
                 // or other visible indicator to the page so that the user is  
                 // informed that they have entered the wrong login info.
-                return null
+                return null;
             }
 
             window.localStorage.setItem("login-data", JSON.stringify(loginData));
-            window.location.assign("/posts.html");  // redirect
+            document.getElementById("successMessage").style.display = "block";
+            setTimeout(() => {
+                window.location.assign("/index.html");  // redirect after showing success message
+            }, 2000);
 
             return loginData;
         });
 }
 
-
-// This is the `logout()` function you will use for any logout button
+// This is the logout() function you will use for any logout button
 // which you may include in various pages in your app. Again, READ this
 // function and you will probably want to re-use parts of it for other
-// `fetch()` requests you may need to write.
+// fetch() requests you may need to write.
 function logout () {
     const loginData = getLoginData();
 
@@ -73,10 +67,6 @@ function logout () {
     const options = { 
         method: "GET",
         headers: { 
-            // This header is how we authenticate our user with the
-            // server for any API requests which require the user
-            // to be logged-in in order to have access.
-            // In the API docs, these endpoints display a lock icon.
             Authorization: `Bearer ${loginData.token}`,
         },
     };
@@ -85,10 +75,6 @@ function logout () {
         .then(response => response.json())
         .then(data => console.log(data))
         .finally(() => {
-            // We're using `finally()` so that we will continue with the
-            // browser side of logging out (below) even if there is an 
-            // error with the fetch request above.
-
             window.localStorage.removeItem("login-data");  // remove login data from LocalStorage
             window.location.assign("/");  // redirect back to landing page
         });
--- scripts/landing.js
+++ scripts/landing.js
@@ -1,24 +1,55 @@
-/* Landing Page JavaScript */
-
 "use strict";
 
-const loginForm = document.querySelector("#login");
+const registerForm = document.querySelector("#registerForm");
 
-loginForm.onsubmit = function (event) {
+registerForm.onsubmit = function (event) {
     // Prevent the form from refreshing the page,
     // as it will do by default when the Submit event is triggered:
     event.preventDefault();
 
-    // We can use loginForm.username (for example) to access
+    // We can use registerForm.username (for example) to access
     // the input element in the form which has the ID of "username".
-    const loginData = {
-        username: loginForm.username.value,
-        password: loginForm.password.value,
+    const registerData = {
+        username: registerForm.username.value,
+        fullName: registerForm.fullName.value,
+        password: registerForm.password.value,
     }
 
     // Disables the button after the form has been submitted already:
-    loginForm.loginButton.disabled = true;
+    registerForm.registerButton.disabled = true;
 
-    // Time to actually process the login using the function from auth.js!
-    login(loginData);
+    // Time to actually process the registration using the function from auth.js!
+    register(registerData);
 };
+
+function register(registerData) {
+    // POST /api/users
+    const options = { 
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(registerData),
+    };
+
+    return fetch(apiBaseURL + "/api/users", options)
+        .then(response => response.json())
+        .then(data => {
+            if (data.error) {
+                console.error(data.error);
+                // Here is where you might want to add an error notification 
+                // or other visible indicator to the page so that the user is  
+                // informed that there was an issue with their registration.
+                return null;
+            }
+
+            // If registration is successful, log the user in
+            login({
+                username: registerData.username,
+                password: registerData.password,
+            });
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
